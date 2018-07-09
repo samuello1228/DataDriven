72da6db55ff4727c6d28a2562ee34451b3f9264f
Samuel Lo <locheukyee2001@gmail.com>
2018-07-09 18:52:11 +0800

    draw hEff_eta and hEff_pt

--- FakeRate/scripts/getFakeEffs/getFakeEffs.C
+++ FakeRate/scripts/getFakeEffs/getFakeEffs.C
@@ -37,6 +37,7 @@ void GetEffs(TString lepton_type,unsigned int NETA,const double ETA[],unsigned i
     TH2D *hTight_prompt_mc = (TH2D*) mc->Get(lepton_type+"_prompt_hTight");
     TH2D *hLoose_prompt_mc = (TH2D*) mc->Get(lepton_type+"_prompt_hLoose");
     
+    //For hEff
     {
         //hEff = numerator = hTight_data - hTight_prompt_mc
         hEff->Add(hTight_data);
@@ -56,9 +57,73 @@ void GetEffs(TString lepton_type,unsigned int NETA,const double ETA[],unsigned i
         delete hTemp;
     }
     
-    delete data;
-    delete mc;
+    //hEff_eta
+    TH1D* hEff_eta = new TH1D(lepton_type+"_hEff_eta", ";|#eta|", NETA, ETA);
+    {
+        TH2D *hTight_data_eta = (TH2D*) hTight_data->RebinX(NPT,lepton_type+"_hTight_eta");
+        TH2D *hLoose_data_eta = (TH2D*) hLoose_data->RebinX(NPT,lepton_type+"_hLoose_eta");
+        TH2D *hTight_prompt_mc_eta = (TH2D*) hTight_prompt_mc->RebinX(NPT,lepton_type+"_prompt_hTight_eta");
+        TH2D *hLoose_prompt_mc_eta = (TH2D*) hLoose_prompt_mc->RebinX(NPT,lepton_type+"_prompt_hLoose_eta");
+        
+        //hEff = numerator = hTight_data - hTight_prompt_mc
+        hTight_prompt_mc_eta->Scale(-1);
+        hTight_data_eta->Add(hTight_prompt_mc_eta);
+        
+        //denominator = hLoose_data - hLoose_prompt_mc
+        hLoose_prompt_mc_eta->Scale(-1);
+        hLoose_data_eta->Add(hLoose_prompt_mc_eta);
+        
+        //hEff = hEff / denominator
+        hTight_data_eta->Divide(hLoose_data_eta);
+        
+        for(unsigned int i=1;i<=NETA;i++)
+        {
+            cout<<hTight_data_eta->GetBinContent(1,i)<<", "<<hTight_data_eta->GetBinError(1,i)<<endl;
+            hEff_eta->SetBinContent(i,hTight_data_eta->GetBinContent(1,i));
+            hEff_eta->SetBinError(i,hTight_data_eta->GetBinError(1,i));
+        }
+        cout<<endl;
+        
+        delete hTight_data_eta;
+        delete hLoose_data_eta;
+        delete hTight_prompt_mc_eta;
+        delete hLoose_prompt_mc_eta;
+    }
     
+    //For hEff_pt
+    TH1D* hEff_pt = new TH1D(lepton_type+"_hEff_pt", ";p_{T} [GeV]", NPT, PT);
+    {
+        TH2D *hTight_data_pt = (TH2D*) hTight_data->RebinY(NETA,lepton_type+"_hTight_pt");
+        TH2D *hLoose_data_pt = (TH2D*) hLoose_data->RebinY(NETA,lepton_type+"_hLoose_pt");
+        TH2D *hTight_prompt_mc_pt = (TH2D*) hTight_prompt_mc->RebinY(NETA,lepton_type+"_prompt_hTight_pt");
+        TH2D *hLoose_prompt_mc_pt = (TH2D*) hLoose_prompt_mc->RebinY(NETA,lepton_type+"_prompt_hLoose_pt");
+        
+        //hEff = numerator = hTight_data - hTight_prompt_mc
+        hTight_prompt_mc_pt->Scale(-1);
+        hTight_data_pt->Add(hTight_prompt_mc_pt);
+        
+        //denominator = hLoose_data - hLoose_prompt_mc
+        hLoose_prompt_mc_pt->Scale(-1);
+        hLoose_data_pt->Add(hLoose_prompt_mc_pt);
+        
+        //hEff = hEff / denominator
+        hTight_data_pt->Divide(hLoose_data_pt);
+        
+        for(unsigned int i=1;i<=NPT;i++)
+        {
+            cout<<hTight_data_pt->GetBinContent(i,1)<<", "<<hTight_data_pt->GetBinError(i,1)<<endl;
+            hEff_pt->SetBinContent(i,hTight_data_pt->GetBinContent(i,1));
+            hEff_pt->SetBinError(i,hTight_data_pt->GetBinError(i,1));
+        }
+        cout<<endl;
+        
+        delete hTight_data_pt;
+        delete hLoose_data_pt;
+        delete hTight_prompt_mc_pt;
+        delete hLoose_prompt_mc_pt;
+    }
+    
+    //For h2
     TH1D* h2[NETA+1];
     for(unsigned int j=1;j<=NETA;j++)
     {
@@ -142,6 +207,55 @@ void GetEffs(TString lepton_type,unsigned int NETA,const double ETA[],unsigned i
         delete h2[j];
     }
     delete c2;
+    
+    //For hEff_eta and hEff_pt
+    hEff_eta->SetLineColor(kBlack);
+    hEff_eta->SetMarkerStyle(20);
+    hEff_eta->SetMarkerColor(kBlack);
+    
+    hEff_pt->SetLineColor(kBlack);
+    hEff_pt->SetMarkerStyle(20);
+    hEff_pt->SetMarkerColor(kBlack);
+    
+    hEff_eta->GetXaxis()->SetTitle("|#eta|");
+    hEff_eta->GetYaxis()->SetRangeUser(0,1);
+    
+    hEff_pt->GetXaxis()->SetTitle("p_{T} [GeV]");
+    hEff_pt->GetYaxis()->SetRangeUser(0,1);
+    
+    if(lepton_type=="El")
+    {
+        hEff_eta->GetYaxis()->SetTitle("Electron Fake Efficiency");
+        hEff_pt->GetYaxis()->SetTitle("Electron Fake Efficiency");
+    }
+    else if(lepton_type=="Mu")
+    {
+        hEff_eta->GetYaxis()->SetTitle("Muon Fake Efficiency");
+        hEff_pt->GetYaxis()->SetTitle("Muon Fake Efficiency");
+    }
+    
+    c2 = new TCanvas();
+    c2->cd();
+    hEff_eta->Draw();
+    ATLASLabel(0.2,0.88,"Internal");
+    NameTemp = lepton_type + "_hEff_eta";
+    NameTemp += ".eps";
+    c2->Print(NameTemp.Data(),"eps");
+    delete hEff_eta;
+    delete c2;
+    
+    c2 = new TCanvas();
+    c2->cd();
+    hEff_pt->Draw();
+    ATLASLabel(0.2,0.88,"Internal");
+    NameTemp = lepton_type + "_hEff_pt";
+    NameTemp += ".eps";
+    c2->Print(NameTemp.Data(),"eps");
+    delete hEff_pt;
+    delete c2;
+    
+    delete data;
+    delete mc;
 }
 
 void getFakeEffs()
