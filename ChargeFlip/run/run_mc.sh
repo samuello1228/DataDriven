e7c96b6b549db4312bf8cfb1e1bf44fa343ada19
Samuel <locheukyee2001@gmail.com>
2019-05-07 18:08:00 +0800

    run signal only

--- ChargeFlip/run/run_mc.sh
+++ ChargeFlip/run/run_mc.sh
@@ -1,4 +1,4 @@
 ./charge_flip mc signal   ../share/mc.txt mc_signal
-./charge_flip mc baseline ../share/mc.txt mc_baseline
+#./charge_flip mc baseline ../share/mc.txt mc_baseline
 #./charge_flip mc signal   ../share/mc_update.txt mc_signal
 #./charge_flip mc baseline ../share/mc_update.txt mc_baseline
